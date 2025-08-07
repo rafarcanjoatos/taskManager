e52894bcaec83c82621c7258de0de9ef7f06d296
Rafael Arcanjo <88547716+rafarcanjoatos@users.noreply.github.com>
2025-08-07 15:52:06 -0300

    Correção da porta e do nome do pacote no Dockerfille, entrypoint e compose. Ajuste no Readme paracondizer com o novo Dockerfille. Correção dos relacionamentos entre as classes. Criação do AbstractTask, para ser estendido nas duas classes que possuem a mesma estrutura.

---
+++ src/main/java/com/ipass/taskManager/model/AbstractTask.java
@@ -0,0 +1,59 @@
+package com.ipass.taskManager.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import org.hibernate.annotations.CreationTimestamp;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@MappedSuperclass
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+public abstract class AbstractTask {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @NotBlank
+    @Column(length = 100)
+    @Size(max = 100)
+    private String titulo;
+
+    @Column(length = 500)
+    @Size(max = 500)
+    private String descricao;
+
+    @Enumerated(EnumType.STRING)
+    private TaskStatus status = TaskStatus.PENDENTE;
+
+    @Column(name = "data_criacao", updatable = false)
+    @CreationTimestamp
+    private LocalDateTime dataCriacao;
+
+    @Column(name = "data_conclusao")
+    private LocalDateTime dataConclusao;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+}
